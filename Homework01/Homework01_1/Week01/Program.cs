22a3225a22ad579d191057c352ad61f2027340a3
1020850886 <43142869+1020850886@users.noreply.github.com>
2018-09-12 13:21:08 +0800

    111
    第一周的作业

---
+++ Homework01/Homework01_1/Week01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            string a = Console.ReadLine();
+            Console.Write("b=");
+            string b = Console.ReadLine();
+            double x = Convert.ToDouble(a);
+            double y = Convert.ToDouble(b);
+            Console.WriteLine("a*b=" + (x * y));
+        }
+
+
+    }
+}
