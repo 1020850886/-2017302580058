fc21c4e7ca735d5c2083a50347b47cb87925bddf
1020850886 <43142869+1020850886@users.noreply.github.com>
2018-09-26 12:11:35 +0800

    用简单工厂模式创建图形并求面积
    333

---
+++ Homework03/Homework03/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework03
+{
+    //所有子类共同的父类
+    abstract class Shape
+    {
+        public double GetNum1(string arg)
+        {
+            double Num1 = double.Parse(arg);
+            return Num1;
+        }
+        public double GetNum2(string arg)
+        {
+            double Num2 = double.Parse(arg);
+            return Num2;
+        }
+
+        public abstract double AreaCalculation(double num1 ,double num2 );
+    }
+
+    //正方形
+    class Square : Shape
+    {
+        override public double AreaCalculation(double num1,double num2)
+        {
+            double area = num1*num2;
+            return area;
+        }
+
+    }
+
+    //三角形
+    class Triangle : Shape
+    {
+        override public double AreaCalculation(double num1, double num2)
+        {
+            double area = num1 * num2 * 0.5;
+            return area;
+        }
+
+    }
+
+
+    //圆形
+    class Roundness : Shape
+    {
+        override public double AreaCalculation(double num1, double num2)
+        {
+            double area = num1 * num2 * 3.14;
+            return area;
+        }
+
+    }
+
+    //长方形
+    class Rectangle : Shape
+    {
+        override public double AreaCalculation(double num1, double num2)
+        {
+            double area = num1 * num2 ;
+            return area;
+        }
+
+    }
+
+    //工厂类
+    class Factory
+    {
+        public static Shape GetShape(String arg)
+        {
+            Shape shape = null;
+            switch (arg)
+            {
+                case "Square":
+                    shape = new Square();
+                    Console.WriteLine("请重复输入两次边长:");
+                    break;
+                case "Triangle":
+                    shape = new Triangle();
+                    Console.WriteLine("请分别输入底和高:");
+                    break;
+                case "Roundness":
+                    shape = new Roundness();
+                    Console.WriteLine("请输入两次半径:");
+                    break;
+                case "Rectangle":
+                    shape = new Rectangle();
+                    Console.WriteLine("请分别输入长和宽:");
+                    break;
+            }
+            return shape;
+        }
+    }
+}
---
+++ Homework03/Homework03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Homework03
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape;
+            Console.WriteLine("Square代表正方形 Triangle代表三角形 Roundness代表圆形 Rectangle代表长方形");
+            Console.WriteLine(" 请输入形状：");
+
+            shape = Factory.GetShape(Console.ReadLine());
+            double Num1 = shape.GetNum1(Console.ReadLine());
+            double Num2 = shape.GetNum1(Console.ReadLine());
+            Console.WriteLine("面积大小为：");
+            double area = shape.AreaCalculation(Num1,Num2);
+            Console.WriteLine(area);
+        }
+    }
+}
+           /* label:;
+            Regex re = new Regex("^([Roundness]{9})$");
+            string a = Console.ReadLine();
+            if (!re.IsMatch(a))
+            {
+                Console.WriteLine("错误！请在给定范围内重新输入");
+                goto label;
+            }*/
